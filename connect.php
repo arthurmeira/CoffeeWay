595a8066f50ff900844962f3ccaf38fd13589c50
Arthur Meria <arthurmeiraprog@gmail.com>
2022-06-16 11:56:39 -0300

    Passing the information recorded on the page to the database.

--- connect.php
+++ connect.php
@@ -4,13 +4,18 @@
     $username = "root";
     $password = "";
     $database = "coffeeway";
-    
-    $conn = mysqli_connect($servername, $username, $password, $database);
 
-    if (!$conn){
-        die(('Connection failed: ') . mysqli_connect_error());
+    function connect(){
+        global $servername, $username, $password, $database;
+        $conn = mysqli_connect($servername, $username, $password, $database);
+
+        if (!$conn){
+            die(('Connection failed: ') . mysqli_connect_error());
+        }
+
+        echo 'Connected successfully.';
+        mysqli_close($conn);
     }
-    echo 'Connected successfully.';
-    mysqli_close($conn);
-    
+    return connect();
+
 ?>
---
+++ user.php
@@ -0,0 +1,54 @@
+<?php
+
+    include_once('connect.php');
+
+    $name_user = $_POST['nome_usuario'];
+    $lastname_user = $_POST['sobrenome_usuario'];
+    $email_user = $_POST['email_usuario'];
+    $password_user = $_POST['senha_usuario'];
+    $cpf_user = $_POST['cpf_usuario'];
+    $rg_user = $_POST['rg_usuario'];
+    $cel_user = $_POST['cel_usuario'];
+    $uf_user = $_POST['uf_usuario'];
+    $city_user = $_POST['cidade_usuario'];
+    $district_user = $_POST['bairro_usuario'];
+    $address_user = $_POST['endereco_usuario'];
+    $birth_date_user = $_POST['dtNasc_usuario']; 
+
+    $conn = connect();
+    $sql = "INSERT INTO usuarios
+            (
+                `id_usuario`, 
+                `nome_usuario`, 
+                `sobrenome_usuario`, 
+                `cpf_usuario`, 
+                `rg_usuario`, 
+                `email_usuario`, 
+                `celular_usuario`, 
+                `uf_usuario`, 
+                `cidade_usuario`, 
+                `bairro_usuario`, 
+                `endereco_usuario`, 
+                `dtNasc_usuario`, 
+                `dtCad_usuario`,
+                `senha_usuario`
+            ) 
+
+            VALUES
+            (   
+                '$name_user',
+                '$lastname_user',
+                '$cpf_user',
+                '$rg_user',
+                '$email_user',
+                '$cel_user',
+                '$uf_user',
+                '$city_user',
+                '$district_user',
+                '$address_user',
+                '$birth_date_user',
+                '$password_user',
+            )";
+            
+
+?>
