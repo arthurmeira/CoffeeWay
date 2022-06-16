32de48e1386dc931793ea15feaa0075d65fdf0b2
Arthur Meria <arthurmeiraprog@gmail.com>
2022-06-16 10:51:57 -0300

    Connecting the project to the database.

--- connect.php
+++ connect.php
@@ -1,5 +1,25 @@
 <?php
 
-    
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "coffeeway";
 
+    /*
+    $conn = mysqli_connect($servername, $username, $password, $database);
+    $conn = mysqli_connect($servername, $username, $password, $database);
+
+    if (!$conn){
+        die(('Connection failed: ') . mysqli_connect_error());
+    }
+    echo 'Connected successfully.';
+    mysqli_close($conn);
+'   */
+
+    if (!$conn){
+        die(('Connection failed: ') . mysqli_connect_error());
+    }
+    echo 'Connected successfully.';
+    mysqli_close($conn);
+    
 ?>
