d52fceaa87ddd073394b9ebbb050e2c2030af92a
Arthur Meria <arthurmeiraprog@gmail.com>
2022-06-17 02:31:59 -0300

    Ajeitando as coisas por aqui.

--- connect.php
+++ connect.php
@@ -5,17 +5,13 @@
     $password = "";
     $database = "coffeeway";
 
-    function connect(){
         global $servername, $username, $password, $database;
-        $conn = mysqli_connect($servername, $username, $password, $database);
+        $conn = new mysqli($servername, $username, $password, $database);
 
-        if (!$conn){
-            die(('Connection failed: ') . mysqli_connect_error());
+        if (!$conn) {
+            die('Not connected: ' . mysqli_connect_error());
+        }else{
+            echo 'Connected <br>';
         }
-
-        echo 'Connected successfully.';
-        mysqli_close($conn);
-    }
-    return connect();
 
 ?>
