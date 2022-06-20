3bb84d534d3109b0dc15f82735df10e4a709b97b
Arthur Meria <arthurmeiraprog@gmail.com>
2022-06-20 01:15:52 -0300

    Continuando o CRUD

--- read.php
+++ read.php
@@ -13,50 +13,57 @@
     <div class="logo">
         <h1>CoffeeWay</h1>
     </div><!--logo-->
-
-    <hr>
 </header>
 
 <section>
 
     <div class="container">
         <div class="card">
+            <fieldset>
+                <h2>Usuários</h3>
+                <hr>
+                
+                <table>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Sobrenome</th>
+                        <th>E-mail</th>
+                        <th>Celular</th>
+                        <th>UF</th>
+                        <th>Cidade</th>
+                        <th>Bairro</th>
+                        <th>Endereço</th>
+                        <th>Data aniversário</th>
+                        <th>Data cadastro</th>
+                    </tr>
+                    <tr>
+                        <td>16</td>
+                        <td>Arthur</td>
+                        <td>Meira</td>
+                        <td>arthurmeiraprog@gmail.com</td>
+                        <td>48999414764</td>
+                        <td>SC</td>
+                        <td>Criciúma</td>
+                        <td>MariaDB</td>
+                        <td>Rua Santo Javascript, 440</td>
+                        <td>20-02-2004</td>
+                        <td>18-11-2021</td>
 
-            <h2>Usuários</h3>
-            <hr>
-            <table>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>Sobrenome</th>
-                    <th>E-mail</th>
-                    <th>Celular</th>
-                    <th>UF</th>
-                    <th>Cidade</th>
-                    <th>Bairro</th>
-                    <th>Endereço</th>
-                    <th>Data aniversário</th>
-                    <th>Data cadastro</th>
-                </tr>
-                <tr>
-                    <td>16</td>
-                    <td>Arthur</td>
-                    <td>Meira</td>
-                    <td>arthurmeiraprog@gmail.com</td>
-                    <td>48999414764</td>
-                    <td>SC</td>
-                    <td>Criciúma</td>
-                    <td>MariaDB</td>
-                    <td>Rua Santo Javascript, 440</td>
-                    <td>20-02-2004</td>
-                    <td>18-11-2021</td>
-                </tr>
-            </table>
+                        <div class="btn" >
+                            <td><button>Edit</button></td>
+                            <td><button>Delete</button></td>
+                        </div><!--btn-->
+                        
+                    </tr>
+                </table>
 
+            </fieldset>
         </div><!--card-->
     </div><!--container-->
 
 </section>
+
 
 
 </body>
